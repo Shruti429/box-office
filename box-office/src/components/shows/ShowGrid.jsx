1172d2816e4fda0d3f88c16b7b3fa2da5103265f
shruti <shrutiomar429@gmail.com>
2022-02-09 18:37:09 +0530

    using custom hooks for starred shows

--- box-office/src/components/shows/ShowGrid.jsx
+++ box-office/src/components/shows/ShowGrid.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable array-callback-return */
 /* eslint-disable react/function-component-definition */
 import React from 'react';
@@ -5,19 +6,34 @@ import ShowCard from './ShowCard';
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 import { FlexGrid } from '../styled';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
--- box-office/src/misc/custom-hooks.jsx
+++ box-office/src/misc/custom-hooks.jsx
@@ -1,33 +1,36 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
-import { useEffect, useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 
 function showsReducer(prevState, action) {
   switch (action.type) {
     case 'ADD': {
       return [...prevState, action.showId];
     }
+
     case 'REMOVE': {
       return prevState.filter(showId => showId !== action.showId);
     }
+
     default:
       return prevState;
   }
 }
 
-function userPersistedReducer(reducer, initialState, key) {
+function usePersistedReducer(reducer, initialState, key) {
   const [state, dispatch] = useReducer(reducer, initialState, initial => {
     const persisted = localStorage.getItem(key);
+
     return persisted ? JSON.parse(persisted) : initial;
   });
 
-  // achive synchonization from local storage
   useEffect(() => {
-    localStorage.getItem(key, JSON.stringify(state));
+    localStorage.setItem(key, JSON.stringify(state));
   }, [state, key]);
+
   return [state, dispatch];
 }
 
 export function useShows(key = 'shows') {
-  return userPersistedReducer(showsReducer, [], key);
+  return usePersistedReducer(showsReducer, [], key);
 }
